0272ad78e52f3b0af93d780cb867608aea591645
Pouria Assadipour <pouriaassadipour@gmail.com>
2019-01-10 03:39:02 -0800

    - helper script to scrub email from commits

---
+++ removeEmail.sh
@@ -0,0 +1,23 @@
+## if you're okay with rewriting history,
+## replace <YOUR_PRIVATE_EMAIL>, <YOUR_NAME>,
+## <YOUR_GITHUB_USERNAME> with your credentials
+## otherwise you'll publicize your email
+for d in */; do
+    cd $d 
+    git filter-branch --env-filter '
+      OLD_EMAIL="<YOUR_PRIVATE_EMAIL>"
+      CORRECT_NAME="<YOUR_NAME>"
+      CORRECT_EMAIL="<YOUR_GITHUB_USERNAME>@users.noreply.github.com"
+      if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+      then
+          export GIT_COMMITTER_NAME="$CORRECT_NAME"
+          export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+      fi
+      if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+      then
+          export GIT_AUTHOR_NAME="$CORRECT_NAME"
+          export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+      fi
+      ' --tag-name-filter cat -- --branches --tags 
+    cd ..
+done

ea60c977400fa65b060a220c5de02e6774206bee
Pouria Assadipour <pouriaa@users.noreply.github.com>
2019-01-10 02:20:26 -0800

    - put it all together

---
+++ index.js
@@ -0,0 +1,23 @@
+const Bitbucket = require("./bin/Bitbucket");
+const Github = require("./bin/Github");
+const dotenv = require("dotenv");
+dotenv.config();
+
+(async () => {
+  // get all the bitbucket repositories we're going to transfer
+  const repositories = await Bitbucket.getRepositories();
+
+  // create a new repository on Github for the repos
+  const successfulCreates = await Github.createRepositories(repositories);
+
+  // clone into a local folder
+  const successfulClones = await Bitbucket.pullRepositories(successfulCreates);
+
+  // push to Github
+  const succesfulPushes = await Github.pushRepositories(successfulClones);
+
+  console.log(
+    "Migrated the following repos sucessfully:\r\n",
+    succesfulPushes.map(repo => repo.slug).join("\r\n")
+  );
+})();
